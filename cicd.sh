a5a41a1137819674c9bb5cb10d5e4e9e6aa4d4d5
Muhamad Alam Syah <55674991+alamsyahh15@users.noreply.github.com>
2024-06-26 17:35:10 +0700

    Create cicd.sh

---
+++ cicd.sh
@@ -0,0 +1,10 @@
+sudo composer update
+sudo composer install
+sudo composer install --optimize-autoloader --no-dev
+sudo npm update
+sudo npm install
+sudo php artisan key:generate
+sudo php artisan optimize
+sudo php artisan route:cache
+sudo php artisan config:cache
+sudo php artisan view:clear
